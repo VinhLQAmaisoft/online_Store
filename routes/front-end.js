5a21e1b18553ede9a7444494cd60f47f45e09773
vinh261100 <vinh261100@gmail.com>
2021-11-02 10:23:30 +0700

    upodate 3

--- routes/front-end.js
+++ routes/front-end.js
@@ -3,34 +3,38 @@ var router = express.Router();
 const axios = require('axios')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('home', { layout: 'layouts/full-page' });
+router.get('/',async function (req, res, next) {
+    const product = await axios({
+        url: process.env.BASE_DOMAIN + '/api/get-all-product',
+        method: 'GET'
+    })
+    res.render('home', { layout: 'layouts/full-page', products:product.data.slice(0,5)});
 });
 /* GET home page. */
-router.get('/about-us', function(req, res, next) {
+router.get('/about-us', function (req, res, next) {
     res.render('about', { title: 'Express' });
 });
 /* GET home page. */
 
-router.get('/blog/:id', function(req, res, next) {
+router.get('/blog/:id', function (req, res, next) {
     res.render('blog-detail', { title: 'Express', layout: 'layouts/full-page' });
 });
 
 /* GET home page. */
-router.get('/gallery', function(req, res, next) {
+router.get('/gallery', function (req, res, next) {
     res.render('gallery', { layout: 'layouts/full-page' });
 });
-router.get('/contacts', function(req, res, next) {
+router.get('/contacts', function (req, res, next) {
     res.render('contacts', { layout: 'layouts/full-page' });
 });
-router.get('/shop', async function(req, res, next) {
+router.get('/shop', async function (req, res, next) {
     const product = await axios({
         url: process.env.BASE_DOMAIN + '/api/get-all-product',
         method: 'GET'
     })
     res.render('shop', { layout: 'layouts/full-page', productList: product.data });
 });
-router.get('/product/:id', async function(req, res, next) {
+router.get('/product/:id', async function (req, res, next) {
     let selectedId = req.params.id;
     const product = await axios({
         method: 'GET',
@@ -39,10 +43,10 @@ router.get('/product/:id', async function(req, res, next) {
     console.log(product.data)
     res.render('shop-detail', { layout: 'layouts/full-page', product: product.data });
 });
-router.get('/checkout', function(req, res) {
+router.get('/checkout', function (req, res) {
     res.render('checkout', { layout: 'layouts/full-page' })
 })
-router.get('/thank-you', function(req, res) {
+router.get('/thank-you', function (req, res) {
     let phone = req.query.phone;
     if (!phone) {
         res.redirect('/')
